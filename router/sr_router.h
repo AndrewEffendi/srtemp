00bbb2537efe846b9849515ae1ef5f69210e0f25
Andrew Effendi <andrewamadeo20@gmail.com>
2024-11-01 11:33:52 -0400

    arrange

--- router/sr_router.h
+++ router/sr_router.h
@@ -67,30 +67,23 @@ int sr_read_from_server(struct sr_instance* );
 /* -- sr_router.c -- */
 void sr_init(struct sr_instance* );
 void sr_handlepacket(struct sr_instance* , uint8_t * , unsigned int , char* );
-
-/* -- sr_if.c -- */
-void sr_add_interface(struct sr_instance* , const char* );
-void sr_set_ether_ip(struct sr_instance* , uint32_t );
-void sr_set_ether_addr(struct sr_instance* , const unsigned char* );
-void sr_print_if_list(struct sr_instance* );
-
-/* Custom methods */
 int validate_checksum(uint8_t *packet, unsigned int offset, unsigned int length, uint16_t old_cksm);
 int check_eth_len(uint8_t *packet, unsigned int len);
 int check_ip_len_cs(uint8_t *pkt, unsigned int len);
 int check_icmp_len_cs(uint8_t *pkt, int len);
 void eth_header(sr_ethernet_hdr_t *eth_hdr, struct sr_if *interface, uint8_t *dest_mac);
 void ip_header(sr_ip_hdr_t *ip_hdr, uint32_t src_ip, uint32_t dst_ip, uint16_t len, uint8_t ttl, uint8_t protocol);
-void send_packet(struct sr_instance*, uint8_t*, unsigned int, struct sr_if*, uint32_t);
-void send_icmp_msg(struct sr_instance*, uint8_t*, unsigned int, uint8_t, uint8_t);
 void handle_arp(struct sr_instance *sr, uint8_t *pkt, char *interface, unsigned int len);
 void handle_ip(struct sr_instance *sr, uint8_t *pkt, unsigned int len, char *interface);
 void forward_ip(struct sr_instance *sr, uint8_t *pkt, unsigned int len);
+void send_packet(struct sr_instance*, uint8_t*, unsigned int, struct sr_if*, uint32_t);
+void send_icmp_msg(struct sr_instance*, uint8_t*, unsigned int, uint8_t, uint8_t);
 struct sr_rt *longest_prefix_match(struct sr_instance *sr, uint32_t dest_addr);
 
-/* Custom method: convert IP int to string */
-void addr_ip_int(char* buf, uint32_t ip);
-int verify_ip(sr_ip_hdr_t*);
-int verify_icmp(uint8_t*, unsigned int);
+/* -- sr_if.c -- */
+void sr_add_interface(struct sr_instance* , const char* );
+void sr_set_ether_ip(struct sr_instance* , uint32_t );
+void sr_set_ether_addr(struct sr_instance* , const unsigned char* );
+void sr_print_if_list(struct sr_instance* );
 
 #endif /* SR_ROUTER_H */
